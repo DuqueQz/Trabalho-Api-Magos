39c8c61a244ede5d8ecfd6f76ec25113ac96fc71
Matheus Togni <matheustogni64@gmail.com>
2024-05-16 19:55:33 -0300

    att no sql

--- db/init.sql
+++ db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE magos (
     mag_bio TEXT
 );
 
--- Tabela de habilidades 
+-- Tabela de habilidades mágicas 
 CREATE TABLE habilidades_magicas (
     hab_id SERIAL PRIMARY KEY,
     hab_nome_da_habilidade VARCHAR(50) NOT NULL,
@@ -17,14 +17,14 @@ CREATE TABLE habilidades_magicas (
     hab_nivel_de_dificuldade INT NOT NULL,
     hab_escola_de_magia VARCHAR(50) NOT NULL,
     hab_dano INT,
-    hab_mana INT
+    hab_mana INT,
+    hab_cooldown INT 
 );
 
 -- Tabela de conquistas
 CREATE TABLE conquistas (
     conq_id SERIAL PRIMARY KEY,
     conq_descricao_da_conquista TEXT NOT NULL,
-    conq_data_da_conquista DATE NOT NULL,
     conq_pontuacao INT NOT NULL,
     conq_tipo VARCHAR(50),
     conq_recompensa TEXT
@@ -34,9 +34,10 @@ CREATE TABLE conquistas (
 CREATE TABLE magos_conquistas (
     mag_id INT NOT NULL,
     conq_id INT NOT NULL,
+    conq_data_da_conquista DATE NOT NULL, 
     PRIMARY KEY (mag_id, conq_id),
-    FOREIGN KEY (mag_id) REFERENCES magos(mag_id) ON DELETE CASCADE,
-    FOREIGN KEY (conq_id) REFERENCES conquistas(conq_id) ON DELETE CASCADE
+    FOREIGN KEY (mag_id) REFERENCES magos(mag_id),
+    FOREIGN KEY (conq_id) REFERENCES conquistas(conq_id)
 );
 
 -- Tabela de interesses
@@ -46,8 +47,8 @@ CREATE TABLE interesses (
     int_id_habilidade INT NOT NULL,
     int_nivel_de_interesse VARCHAR(10) NOT NULL,
     int_data_de_registro DATE NOT NULL,
-    FOREIGN KEY (int_id_mago) REFERENCES magos(mag_id) ON DELETE CASCADE,
-    FOREIGN KEY (int_id_habilidade) REFERENCES habilidades_magicas(hab_id) ON DELETE CASCADE
+    FOREIGN KEY (int_id_mago) REFERENCES magos(mag_id),
+    FOREIGN KEY (int_id_habilidade) REFERENCES habilidades_magicas(hab_id)
 );
 
 -- Tabela de trocas (detalhes da troca)
@@ -56,8 +57,8 @@ CREATE TABLE trocas (
     troca_id_mago_ofertante INT NOT NULL,
     troca_id_mago_interessado INT NOT NULL,
     troca_data DATE NOT NULL,
-    FOREIGN KEY (troca_id_mago_ofertante) REFERENCES magos(mag_id) ON DELETE CASCADE,
-    FOREIGN KEY (troca_id_mago_interessado) REFERENCES magos(mag_id) ON DELETE CASCADE
+    FOREIGN KEY (troca_id_mago_ofertante) REFERENCES magos(mag_id),
+    FOREIGN KEY (troca_id_mago_interessado) REFERENCES magos(mag_id)
 );
 
 -- Tabela de itens_trocados (detalhes do que está sendo trocado)
@@ -70,7 +71,7 @@ CREATE TABLE itens_trocados (
     item_escola_de_magia VARCHAR(50) NOT NULL,
     item_dano INT,
     item_mana INT,
-    FOREIGN KEY (troca_id) REFERENCES trocas(troca_id) ON DELETE CASCADE
+    FOREIGN KEY (troca_id) REFERENCES trocas(troca_id)
 );
 
 -- Tabela de mensagens
@@ -81,7 +82,7 @@ CREATE TABLE mensagens (
     msg_destinatario TEXT NOT NULL,
     msg_conteudo TEXT NOT NULL,
     msg_data_de_envio TIMESTAMP NOT NULL,
-    FOREIGN KEY (msg_id_troca) REFERENCES trocas(troca_id) ON DELETE CASCADE
+    FOREIGN KEY (msg_id_troca) REFERENCES trocas(troca_id)
 );
 
 -- Tabela de avaliações
@@ -92,8 +93,8 @@ CREATE TABLE avaliacoes (
     ava_pontuacao INT NOT NULL,
     ava_comentario TEXT,
     ava_data_da_avaliacao TIMESTAMP NOT NULL,
-    FOREIGN KEY (ava_id_troca) REFERENCES trocas(troca_id) ON DELETE CASCADE,
-    FOREIGN KEY (ava_id_mago_avaliador) REFERENCES magos(mag_id) ON DELETE CASCADE
+    FOREIGN KEY (ava_id_troca) REFERENCES trocas(troca_id),
+    FOREIGN KEY (ava_id_mago_avaliador) REFERENCES magos(mag_id)
 );
 
 -- Tabela de amizades
@@ -103,6 +104,6 @@ CREATE TABLE amizades (
     amiz_id_mago_2 INT NOT NULL,
     amiz_data_do_inicio DATE NOT NULL,
     amiz_status VARCHAR(10) NOT NULL,
-    FOREIGN KEY (amiz_id_mago_1) REFERENCES magos(mag_id) ON DELETE CASCADE,
-    FOREIGN KEY (amiz_id_mago_2) REFERENCES magos(mag_id) ON DELETE CASCADE
+    FOREIGN KEY (amiz_id_mago_1) REFERENCES magos(mag_id),
+    FOREIGN KEY (amiz_id_mago_2) REFERENCES magos(mag_id)
 );
